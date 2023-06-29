9162ae1f8ffa67dbac7436611297772a895f19db
Furkanturanr <furkanturan10r@hotmail.com>
2023-06-29 17:50:52 +0300

    last homework js restaurant menu is  finished

---
+++ js/classworks/restaurant-menu/app.js
@@ -0,0 +1,157 @@
+const menu = [
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img: "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img: "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img: "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img: "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img: "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img: "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img: "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+];
+
+const menuItems = document.querySelector("#menu-items");
+
+//Filtreleme butonlarını oluşturacağımız alanı oluşturuyoruz
+let filterButtons = document.querySelector("#filter-buttons");
+let categories=[]
+categories.push("All")
+
+// Menüdeki kategorileri getirip bunlara göre filtreleme yapılmasını sağlayacağız
+menu.forEach((item) => {
+  if (!categories.includes(item.category)) {
+    categories.push(item.category)
+  }
+})
+
+categories.forEach((category)=>{
+  let filterBtn=document.createElement('button')
+  filterBtn.classList.add('btn','btn-outline-dark','mx-2')
+  filterBtn.textContent=category
+  filterButtons.appendChild(filterBtn)
+
+  if (filterBtn.textContent==="All") {
+    filterBtn.classList.add("active")
+  }
+
+  filterButtons.addEventListener("click", handleFilter)
+  
+})
+
+
+function handleFilter(e) {
+  const selectedCategory = e.target.textContent
+  
+  const removeActiveButtons = filterButtons.querySelectorAll("button")
+  removeActiveButtons.forEach((itemBtn)=>{
+    itemBtn.classList.remove("active")
+  })
+
+  let selectedBtn = e.target
+  selectedBtn.classList.add("active")
+  
+  const newMenuItems=document.querySelectorAll(`#menu-items .col-6`)
+  newMenuItems.forEach((item)=>{
+    if (selectedCategory === "All" || item.classList.contains(selectedCategory)) {
+      item.style.display = "block";
+    }
+    else
+    {
+      item.style.display="none"
+    }
+  })
+
+}
+
+
+menu.forEach((product) => {
+  let itemDiv = document.createElement("div");
+  itemDiv.classList.add("col-6", "mb-3", "All", product.category);
+  itemDiv.innerHTML = `
+    <div class="row">
+      <div class="col-4">
+        <img class="photo" src="${product.img}">
+      </div>
+      <div class="col-8">
+      <div class="row border-bottom border-success">
+      <div class="col-12 d-flex justify-content-between">
+      <h3 class="item-title">${product.title}</h3>
+      <span class="float-end item-price">${product.price} $</span>
+      </div>
+      </div>
+        
+        <p>${product.desc}</p>
+      </div>
+    </div>
+  `;
+
+  menuItems.appendChild(itemDiv);
+});
+
+
+
+
+
